9e6caba17f68eac48f2b1eb0d64d2261732782f3
Chloe (Desktop) <cragzu@gmail.com>
2021-03-06 13:33:49 -0800

    added methods for add/subtract to savings and monthly compound interest

--- src/components/Driver.js
+++ src/components/Driver.js
@@ -10,10 +10,45 @@ class Driver extends React.Component {
             checkingAccountBalance: 100.0,
             currentNetWorth: 100.0,
             currentMonth: 0, // months go from 0-95 for gameplay between 10 and 18 years of age (8yr * 12mo = 96mo)
+            savingsAccountBalance: 0,
             savingsInterestRate: 0.005,
+            tfsaAccountBalance: 0,
             tfsaInterestRate: 0.008,
+            tfsaContributionRoom: 8000,
             // add more values as needed
+        };
+    }
+
+    // Add money from checking acc into savings
+    addBalanceSavings(amount) {
+        if (amount > this.state.checkingAccountBalance) {
+            console.log("Not enough money for this transaction!") // todo: visually display this
         }
+        this.setState({
+            checkingAccountBalance: this.state.checkingAccountBalance - amount,
+            savingsAccountBalance: this.state.savingsAccountBalance + amount
+        });
+    }
+
+    // Add money from savings acc into checking
+    subtractBalanceSavings(amount) {
+        if (amount > this.state.savingsAccountBalance) {
+            console.log("Not enough money for this transaction!") // todo: visually display this
+        }
+        this.setState({
+            checkingAccountBalance: this.state.checkingAccountBalance + amount,
+            savingsAccountBalance: this.state.savingsAccountBalance - amount
+        });
+    }
+
+    // Add interest to cash accounts at the end of the month
+    monthlyCompoundInterestCashAccounts() {
+        let savingsInterest = (this.state.savingsAccountBalance * this.state.savingsAccountBalance) / 12;
+        let tfsaInterest = (this.state.tfsaAccountBalance * this.state.tfsaInterestRate) / 12;
+        this.setState({
+            savingsAccountBalance: this.state.savingsAccountBalance + savingsInterest,
+            tfsaAccountBalance: this.state.tfsaAccountBalance + tfsaInterest
+        });
     }
 
     render() {
