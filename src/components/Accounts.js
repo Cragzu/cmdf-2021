fbc443a29cb6369921a15d7712f7eb504964422c
Kashish Bansal <aashma.2008@gmail.com>
2021-03-07 02:54:11 +0530

    working on accounts component

---
+++ src/components/Accounts.js
@@ -0,0 +1,30 @@
+import react, { Component } from 'react';
+
+class Accounts extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    nextMonth = () => {
+        
+    }
+    render() {
+        return(
+            <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Everyday Savings</h5>
+                <p class="card-text">Balance:           $100.00</p>
+                <p class="card-text">Balance(TSFA):         $80.00</p>
+                <p class="card-text">Contribution room</p>
+                <a href="#" class="card-link">Deposit</a>
+                <a href="#" class="card-link">Withdraw</a>
+            </div>
+            </div>
+             );
+    }
+}
+
+export default Accounts;
