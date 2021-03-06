604e81a53fa1c36b6ef9ffce1d43eecd8b22aa21
Chloe (Desktop) <cragzu@gmail.com>
2021-03-06 11:47:05 -0800

    created initial driver with constructor, updated default app.js

---
+++ src/components/Driver.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Driver extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            checkingAccountBalance: 100.0,
+            currentNetWorth: 100.0,
+            currentMonth: 0 // months go from 0-95 for gameplay between 10 and 18 years of age (8yr * 12mo = 96mo)
+        }
+    }
+
+
+
+
+}
+
+export default Driver;
