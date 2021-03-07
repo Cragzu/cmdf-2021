88e9bb580b691bb5d09c63836f6a1fb17aa98260
jkcadee <jkwok55@my.bcit.ca>
2021-03-07 11:25:21 -0800

    functionality added

--- src/components/Driver.js
+++ src/components/Driver.js
@@ -18,7 +18,10 @@ class Driver extends React.Component {
             tfsaAccountBalance: 0,
             tfsaInterestRate: 0.008,
             tfsaContributionRoom: 8000,
-            // add more values as needed
+            listOfStocks: ["TSLA", "MSFT", "AMZN"],
+            stockVal: [Math.floor(Math.random() * 1001), Math.floor(Math.random() * 1001), Math.floor(Math.random() * 1001)],
+            percentage: [Math.floor(Math.random() * 31), Math.floor(Math.random() * 31), Math.floor(Math.random() * 31)],
+            units: [0, 0, 0]
         };
     }
 
@@ -92,60 +95,132 @@ class Driver extends React.Component {
         });
     }
 
+    sellStocks = (value, index) => {
+        if (this.state.units[index] > 0) {
+            this.state.units[index]--;
+            this.setState({
+                checkingAccountBalance: this.state.checkingAccountBalance + value,
+            });
+        }
+    }
+
+    buyStocks = (value, index) => {
+        if (this.state.checkingAccountBalance >=  value) {
+            this.state.units[index]++;
+            this.setState({
+                checkingAccountBalance: this.state.checkingAccountBalance - value,
+                currentNetWorth: this.state.currentNetWorth + value,
+            });
+        }
+    }
+
+    // resets the stocks each month
+    stockReset = () => {
+        let newStockVal = [Math.floor(Math.random() * 1001), Math.floor(Math.random() * 1001), Math.floor(Math.random() * 1001)];
+        let newPercentage = [Math.floor(Math.random() * 31), Math.floor(Math.random() * 31), Math.floor(Math.random() * 31)];
+
+        this.setState({
+            stockVal: newStockVal,
+            percentage: newPercentage
+        });
+    }
+
+
+
     // do all monthly functions
     moveToNextMonth = () => {
         this.monthlyCompoundInterestCashAccounts();
         this.monthlyGetAllowance();
+        this.stockReset();
     };
 
 
     render() {
         return (
-            <div>
-                <Sidebar
-                    checkingAccountBalance={this.state.checkingAccountBalance.toFixed(2)}
-                    currentNetWorth={this.state.currentNetWorth.toFixed(2)}
-                    currentMonth={this.state.currentMonth}
-                    changeMonthButtonOnClick={this.moveToNextMonth}
-                />
-                <div className={'driver'}>
+            <div className={'Driver'}>
+                <div className="col">
+                    <div className="row">
+                        <div className="col-md-3">
+                        <Sidebar 
+                        checkingAccountBalance={this.state.checkingAccountBalance.toFixed(2)}
+                        currentNetWorth={this.state.currentNetWorth.toFixed(2)}
+                        currentMonth={this.state.currentMonth}
+                        changeMonthButtonOnClick={this.moveToNextMonth}
+                        />
+                        </div>
+
+                        <div className="col-sm">
+                        <CashAccount
+                            accountTypeIsTFSA={false}
+                            savingsAccountBalance={this.state.savingsAccountBalance.toFixed(2)}
+                            addBalanceSavingsOnClick={this.addBalanceSavings}
+                            subtractBalanceSavingsOnClick={this.subtractBalanceSavings}
+                            addBalanceTFSAOnClick={this.addBalanceTFSA}
+                            subtractBalanceTFSAOnClick={this.subtractBalanceTFSA}
+                        />
+                        <CashAccount
+                            accountTypeIsTFSA={true}
+                            tfsaAccountBalance={this.state.tfsaAccountBalance.toFixed(2)}
+                            tfsaContributionRoom={this.state.tfsaContributionRoom.toFixed(2)}
+                            addBalanceSavingsOnClick={this.addBalanceSavings}
+                            subtractBalanceSavingsOnClick={this.subtractBalanceSavings}
+                            addBalanceTFSAOnClick={this.addBalanceTFSA}
+                            subtractBalanceTFSAOnClick={this.subtractBalanceTFSA}
+                        />
+                        <GIC />
+                        </div>
 
 
-                    <CashAccount
-                        accountTypeIsTFSA={false}
-                        savingsAccountBalance={this.state.savingsAccountBalance.toFixed(2)}
-                        addBalanceSavingsOnClick={this.addBalanceSavings}
-                        subtractBalanceSavingsOnClick={this.subtractBalanceSavings}
-                        addBalanceTFSAOnClick={this.addBalanceTFSA}
-                        subtractBalanceTFSAOnClick={this.subtractBalanceTFSA}
+                        <div className="col-md-6">
 
-                    />
-                    <CashAccount
-                        accountTypeIsTFSA={true}
-                        tfsaAccountBalance={this.state.tfsaAccountBalance.toFixed(2)}
-                        tfsaContributionRoom={this.state.tfsaContributionRoom.toFixed(2)}
-                        addBalanceSavingsOnClick={this.addBalanceSavings}
-                        subtractBalanceSavingsOnClick={this.subtractBalanceSavings}
-                        addBalanceTFSAOnClick={this.addBalanceTFSA}
-                        subtractBalanceTFSAOnClick={this.subtractBalanceTFSA}
-                    />
+                        </div>
 
-                    <div style={{paddingTop: "40em"}}>
-                        <GIC/>
-                    </div>
+                            <div className="col-md-4" style={{paddingTop: "10px"}}>
+                                <MutualFunds />
+                            </div>
 
-                    <div className="col-md-4" style={{paddingTop: "40em"}}>
-                        <MutualFunds/>
-                    </div>
+                            <div className="col-md-3" style={{paddingTop: "10px"}}>
+                               
+                            </div>
 
-                    <div className="col-md-4" style={{paddingTop: "40em"}}>
-                        <Stocks/>
+                            <div className="col-md-3" style={{paddingTop: "10px"}} key={this.state.stockVal[0]}>
+                                <Stocks 
+                                listOfStocks={this.state.listOfStocks}
+                                listOfUnits={this.state.units}
+                                numStock={0}
+                                stockVal={this.state.stockVal} 
+                                percentage={this.state.percentage}
+                                buyStocks={this.buyStocks}
+                                sellStocks={this.sellStocks}
+                                />
+                            </div>
+                            <div className="col-md-3" style={{paddingTop: "10px"}} key={this.state.stockVal[1]}>
+                                <Stocks 
+                                listOfStocks={this.state.listOfStocks}
+                                listOfUnits={this.state.units}
+                                numStock={1}
+                                stockVal={this.state.stockVal} 
+                                percentage={this.state.percentage}
+                                buyStocks={this.buyStocks}
+                                sellStocks={this.sellStocks}
+                                />
+                            </div>
+                            <div className="col-md-3" style={{paddingTop: "10px"}} key={this.state.stockVal[2]}>
+                                <Stocks 
+                                listOfStocks={this.state.listOfStocks}
+                                listOfUnits={this.state.units}
+                                numStock={2}
+                                stockVal={this.state.stockVal} 
+                                percentage={this.state.percentage}
+                                buyStocks={this.buyStocks}
+                                sellStocks={this.sellStocks}
+                                />
+                            </div>
                     </div>
 
                 </div>
+
             </div>
-
-
         )
     }
 }
